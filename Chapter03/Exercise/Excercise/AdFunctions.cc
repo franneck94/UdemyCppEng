8f7964d0b5c99dfeca033f614670eb7902e1830c
William Huynh <huyhuynhngoan@gmail.com>
2024-01-10 12:33:59 -0400

    Added to source file as requested

--- Chapter03/Exercise/Excercise/AdFunctions.cc
+++ Chapter03/Exercise/Excercise/AdFunctions.cc
@@ -4,3 +4,27 @@
 
 #include "AdConstants.h"
 #include "AdFunctions.h"
+
+namespace Add
+{
+namespace Visualize
+{
+
+void print_vehicle(const Ad::Types::VehicleType &vehicle)
+{
+
+}
+
+void print_neighbor_vehicles(const Ad::Types::NeighborVehiclesType &vehicles)
+{
+
+}
+
+void print_scene(const Ad::Types::VehicleType &ego_vehicle,
+                 const Ad::Types::NeighborVehiclesType &vehicles)
+{
+
+}
+
+} // namespace Visualize
+} // namespace Add
