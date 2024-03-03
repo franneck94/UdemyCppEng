3a64800102e32270afc5acd9adf59464e8d14730
franneck94 <schaffranek.jan@web.de>
2024-03-03 08:32:58 +0100

    up

--- Chapter05/Exercise/Exercise/main.cc
+++ Chapter05/Exercise/Exercise/main.cc
@@ -10,3 +10,47 @@
 #include "utils.h"
 
 namespace fs = std::filesystem;
+
+using WordVector = std::vector<std::string>;
+using WordCountPair = std::pair<std::string, std::size_t>;
+using CountedWordsMap = std::map<std::string, std::size_t>;
+
+WordVector split_text(const std::string &text, char delimiter);
+
+CountedWordsMap count_words(const WordVector &words);
+
+int main()
+{
+    auto current_path = fs::current_path();
+    current_path /= "text.txt";
+
+    auto text = readFile(current_path.string());
+    std::cout << text << '\n' << '\n';
+
+    auto splitted_text = split_text(text, ' ');
+    print_vector(splitted_text);
+
+    auto counted_words = count_words(splitted_text);
+    print_map(counted_words);
+
+    return 0;
+}
+
+WordVector split_text(const std::string &text, char delimiter)
+{
+    auto words = WordVector{};
+
+    auto iss = std::istringstream{text};
+    auto item = std::string{};
+
+    while (std::getline(iss, item, delimiter))
+    {
+        words.push_back(item);
+    }
+
+    return words;
+}
+
+CountedWordsMap count_words(const WordVector &words)
+{
+}
