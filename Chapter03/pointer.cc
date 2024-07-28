40862d7b3dac5a61514a550ad60b899132ce1fd3
franneck94 <schaffranek.jan@web.de>
2024-07-28 17:18:14 +0200

    up

--- Chapter03/pointer.cc
+++ Chapter03/pointer.cc
@@ -2,27 +2,18 @@
 #include <cstdint>
 #include <iostream>
 
-template <std::size_t N>
-void print_array_values(const std::array<std::uint32_t, N> *arr)
-{
-    std::cout << arr << '\n';
-    for (std::size_t i = 0; i < arr->size(); i++)
-    {
-        std::cout << (*arr)[i] << '\n';
-    }
-}
-
 int main()
 {
-    std::uint32_t value = 42;
+                                      // Address: Value
+    auto my_age = std::uint32_t{30U}; // F940-F943: 30
+    std::cout << &my_age << "\n";
+    std::cout << my_age << "\n";
+                                                   // Address: Value
+    std::uint32_t *pointer_to_some_data = &my_age; // F6E8:    F940
+    std::cout << &pointer_to_some_data << "\n";
 
-    std::uint32_t *value_ptr = &value;
-    std::cout << value_ptr << '\n';
-    std::cout << *value_ptr << '\n';
-
-    auto my_array = std::array<std::uint32_t, 5U>{1, 2, 3, 4, 5};
-    std::cout << &my_array << '\n';
-    print_array_values(&my_array);
+    *pointer_to_some_data = 31; // deref operator
+    std::cout << my_age << "\n";
 
     return 0;
 }
--- Chapter03/strings.cc
+++ Chapter03/strings.cc
@@ -3,17 +3,22 @@
 #include <iostream>
 #include <string>
 
+// "Jan": String literal - known at compile time
 int main()
 {
-    // C-String
-    char my_text1[] = "ABCDEFG"; // \0
+    char single_chatacter = 'a';
+    char my_text1[] = "Jan"; // \0 - null terminating
+    char my_text2[] = {'J', 'a', 'n', '\0'}; // \0 - null terminating
+    auto my_text3 = std::array<char, 4>{"Jan"};
 
-    // C++ Array (Bad)
-    auto my_text2 = std::array<char, 8>{'A', 'B', 'C', 'D'};
+    auto my_text4 = std::string{"Jan"};
+    std::cout << my_text4.size() << "\n";
+    my_text4.append(" Schaffranek");
+    std::cout << my_text4.size() << "\n";
 
-    auto my_text3 = std::string{"ABCDEFG"};
-    std::cout << my_text3.size() << '\n';   // excludes \0
-    std::cout << my_text3.length() << '\n'; // excludes \0
+    // Whats used in modern c++?
+    // my_text1
+    // my_text4
 
     return 0;
 }
