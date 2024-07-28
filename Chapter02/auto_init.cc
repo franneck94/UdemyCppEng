fe1ca7358cf2798affb8d2d0d693d88d072a4e9b
franneck94 <schaffranek.jan@web.de>
2024-07-28 09:06:40 +0200

    up

--- Chapter02/auto_init.cc
+++ Chapter02/auto_init.cc
@@ -1,32 +1,16 @@
 #include <iostream>
 
-enum class Status
-{
-    Unkown,
-    Connected,
-    Disconnected,
-};
-
-// POD
-struct User
-{
-    Status status;
-    std::uint64_t id;
-};
-
 // AAA: Almost Always Auto
 // DRY: Dont Repeat Yourself
 int main()
 {
-    float a = 10.0F;
+    float a = 10.5; // implicity converted
 
-    // std::int32_t b = a;
-    // auto c = std::int32_t{a};
-    auto d = static_cast<std::int32_t>(a);
+    std::int32_t b = a; // implicity converted
+    auto c = static_cast<std::int32_t>(a); // explicitly converted
 
-    auto e = std::int32_t{a};
-
-    auto user1 = User{.status = Status::Connected, .id = 42U}; // C++20
+    // auto d = std::int32_t{a}; // uniform init
+    auto e = std::int32_t{}; // uniform init
 
     return 0;
 }
