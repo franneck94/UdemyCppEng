13718d53795d3f6eb32ba586a6c8a1a97669949b
franneck94 <schaffranek.jan@web.de>
2024-07-28 12:54:01 +0200

    up

--- Chapter03/array.cc
+++ Chapter03/array.cc
@@ -2,34 +2,33 @@
 #include <cstdint>
 #include <iostream>
 
-// c style array as param => it will decay to a pointer (64bits = 8bytes)
-void print_array_values1(const std::uint32_t arr[], const std::size_t len)
+// c-style array decays to a pointer when passed into a function
+void print_array_values1(const std::uint32_t *my_array, const std::uint32_t len)
 {
-    for (std::size_t i = 0; i < len; i++)
+    for (std::uint32_t i = 0; i < len; i++)
     {
-        std::cout << arr[i] << '\n';
+        std::cout << my_array[i] << "\n";
     }
 }
 
-template <std::size_t N>
-void print_array_values2(const std::array<std::uint32_t, N> arr)
+// c++-style arrays
+template <std::uint32_t N>
+void print_array_values2(const std::array<std::uint32_t, N> my_array)
 {
-    for (std::size_t i = 0; i < arr.size(); i++)
+    for (std::uint32_t i = 0; i < my_array.size(); i++)
     {
-        std::cout << arr[i] << '\n';
+        std::cout << my_array[i] << "\n";
     }
 }
 
 int main()
 {
-    constexpr static auto len = std::size_t{5};
+    constexpr auto len = 5U;
 
-    // C-Style Array
-    std::uint32_t my_array[len] = {1, 2, 3, 4, 5};
+    std::uint32_t my_array[len] = {1, 2, 3, 4, 5}; // c-style
     print_array_values1(my_array, len);
 
-    // C++ Array
-    auto my_array2 = std::array<std::uint32_t, len>{1, 2, 3, 4, 5};
+    auto my_array2 = std::array<std::uint32_t, len>{6, 7, 8, 9, 10};
     print_array_values2(my_array2);
 
     return 0;
