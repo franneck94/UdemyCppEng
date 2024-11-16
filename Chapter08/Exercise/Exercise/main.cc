c7fdeab0bbe7fda6792c2440beb17339ed3c823a
franneck94 <schaffranek.jan@web.de>
2024-11-16 13:53:39 +0100

    fix

--- Chapter08/Exercise/Exercise/main.cc
+++ Chapter08/Exercise/Exercise/main.cc
@@ -5,26 +5,6 @@
 #include <type_traits>
 
 template <typename StringType, typename ViewType>
-struct is_char_based
-    : public std::conjunction<std::is_same<std::string, StringType>,
-                              std::is_same<std::string_view, ViewType>>
-{
-};
-
-template <typename StringType, typename ViewType>
-struct is_wchar_based
-    : public std::conjunction<std::is_same<std::wstring, StringType>,
-                              std::is_same<std::wstring_view, ViewType>>
-{
-};
-
-template <typename StringType, typename ViewType>
-struct is_string : public std::disjunction<is_char_based<StringType, ViewType>,
-                                           is_wchar_based<StringType, ViewType>>
-{
-};
-
-template <typename StringType, typename ViewType>
 StringType to_upper_case(ViewType text);
 
 template <typename StringType, typename ViewType>
